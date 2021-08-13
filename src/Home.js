a0a88a61fb67db664dda25cd8c64625e5c1e64fc
chu-young <ttheko101@gmail.com>
2021-08-13 16:18:16 +0200

    project ready for start

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import styles from './Home.module.css';
+
+function Home() {
+    return (
+   <div className="page">
+     <h1>Hello world!</h1>
+   </div>
+    );
+  }
+  
+  export default Home;
