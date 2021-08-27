a37b8e59ef26fc0300dd7e1efd8b77abf9e7c1f2
chu-young <ttheko101@gmail.com>
2021-08-27 15:03:47 +0200

    page complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Home from './Home'
-import './App.css';
-import ParticleBackground from './ParticleBackground';
+
+
 
 function App() {
   return (
     <div>
-    <ParticleBackground/>
     <Home/>
-
  </div>
   );
 }
